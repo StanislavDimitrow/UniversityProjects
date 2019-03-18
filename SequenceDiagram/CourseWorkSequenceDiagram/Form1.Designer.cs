82f1d0544e1ebffcaa0b8691243352b0da7640c4
StanislavDimitrow <47104868+StanislavDimitrow@users.noreply.github.com>
2019-03-18 15:50:39 +0200

    Add files via upload

---
+++ SequenceDiagram/CourseWorkSequenceDiagram/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace CourseWorkSequenceDiagram
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel = new System.Windows.Forms.Panel();
+            this.btnTextFont = new System.Windows.Forms.Button();
+            this.btnDashLine = new System.Windows.Forms.Button();
+            this.btnNewRect = new System.Windows.Forms.Button();
+            this.btnArc = new System.Windows.Forms.Button();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.tracker1 = new System.Windows.Forms.TrackBar();
+            this.btnActor = new System.Windows.Forms.Button();
+            this.btnText = new System.Windows.Forms.Button();
+            this.btnFreehand = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.objectMove = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tracker1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(815, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem1,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.newToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem1
+            // 
+            this.newToolStripMenuItem1.Name = "newToolStripMenuItem1";
+            this.newToolStripMenuItem1.Size = new System.Drawing.Size(120, 22);
+            this.newToolStripMenuItem1.Text = "New";
+            this.newToolStripMenuItem1.Click += new System.EventHandler(this.newToolStripMenuItem1_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As..";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.GhostWhite;
+            this.panel.Controls.Add(this.objectMove);
+            this.panel.Controls.Add(this.btnTextFont);
+            this.panel.Controls.Add(this.btnDashLine);
+            this.panel.Controls.Add(this.btnNewRect);
+            this.panel.Controls.Add(this.btnArc);
+            this.panel.Controls.Add(this.btnRectangle);
+            this.panel.Controls.Add(this.btnLine);
+            this.panel.Controls.Add(this.tracker1);
+            this.panel.Controls.Add(this.btnActor);
+            this.panel.Controls.Add(this.btnText);
+            this.panel.Controls.Add(this.btnFreehand);
+            this.panel.Location = new System.Drawing.Point(0, 27);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(247, 469);
+            this.panel.TabIndex = 1;
+            // 
+            // btnTextFont
+            // 
+            this.btnTextFont.Location = new System.Drawing.Point(134, 339);
+            this.btnTextFont.Name = "btnTextFont";
+            this.btnTextFont.Size = new System.Drawing.Size(75, 23);
+            this.btnTextFont.TabIndex = 10;
+            this.btnTextFont.Text = "TextFont";
+            this.btnTextFont.UseVisualStyleBackColor = true;
+            this.btnTextFont.Click += new System.EventHandler(this.btnTextFont_Click);
+            // 
+            // btnDashLine
+            // 
+            this.btnDashLine.Image = global::CourseWorkSequenceDiagram.Properties.Resources.dashLine;
+            this.btnDashLine.Location = new System.Drawing.Point(26, 220);
+            this.btnDashLine.Name = "btnDashLine";
+            this.btnDashLine.Size = new System.Drawing.Size(75, 23);
+            this.btnDashLine.TabIndex = 9;
+            this.btnDashLine.UseVisualStyleBackColor = true;
+            this.btnDashLine.Click += new System.EventHandler(this.btnDashLine_Click);
+            // 
+            // btnNewRect
+            // 
+            this.btnNewRect.Image = global::CourseWorkSequenceDiagram.Properties.Resources.new_rects;
+            this.btnNewRect.Location = new System.Drawing.Point(165, 179);
+            this.btnNewRect.Name = "btnNewRect";
+            this.btnNewRect.Size = new System.Drawing.Size(44, 64);
+            this.btnNewRect.TabIndex = 8;
+            this.btnNewRect.UseVisualStyleBackColor = true;
+            this.btnNewRect.Click += new System.EventHandler(this.btnNewRect_Click);
+            // 
+            // btnArc
+            // 
+            this.btnArc.Image = global::CourseWorkSequenceDiagram.Properties.Resources.line1;
+            this.btnArc.Location = new System.Drawing.Point(133, 41);
+            this.btnArc.Name = "btnArc";
+            this.btnArc.Size = new System.Drawing.Size(76, 36);
+            this.btnArc.TabIndex = 7;
+            this.btnArc.UseVisualStyleBackColor = true;
+            this.btnArc.Click += new System.EventHandler(this.btnArc_Click);
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Image = global::CourseWorkSequenceDiagram.Properties.Resources.rectangles;
+            this.btnRectangle.Location = new System.Drawing.Point(133, 113);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(76, 37);
+            this.btnRectangle.TabIndex = 6;
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // btnLine
+            // 
+            this.btnLine.Image = global::CourseWorkSequenceDiagram.Properties.Resources.line;
+            this.btnLine.Location = new System.Drawing.Point(25, 179);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(76, 21);
+            this.btnLine.TabIndex = 5;
+            this.btnLine.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // tracker1
+            // 
+            this.tracker1.Location = new System.Drawing.Point(24, 339);
+            this.tracker1.Name = "tracker1";
+            this.tracker1.Size = new System.Drawing.Size(104, 45);
+            this.tracker1.TabIndex = 4;
+            this.tracker1.Scroll += new System.EventHandler(this.tracker1_Scroll);
+            // 
+            // btnActor
+            // 
+            this.btnActor.BackgroundImage = global::CourseWorkSequenceDiagram.Properties.Resources.ddddd;
+            this.btnActor.Location = new System.Drawing.Point(24, 41);
+            this.btnActor.Name = "btnActor";
+            this.btnActor.Size = new System.Drawing.Size(62, 109);
+            this.btnActor.TabIndex = 3;
+            this.btnActor.UseVisualStyleBackColor = true;
+            this.btnActor.Click += new System.EventHandler(this.btnActor_Click);
+            // 
+            // btnText
+            // 
+            this.btnText.Location = new System.Drawing.Point(26, 275);
+            this.btnText.Name = "btnText";
+            this.btnText.Size = new System.Drawing.Size(75, 23);
+            this.btnText.TabIndex = 1;
+            this.btnText.Text = "Text";
+            this.btnText.UseVisualStyleBackColor = true;
+            this.btnText.Click += new System.EventHandler(this.btnText_Click);
+            this.btnText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.btnText_KeyPress);
+            // 
+            // btnFreehand
+            // 
+            this.btnFreehand.Location = new System.Drawing.Point(134, 275);
+            this.btnFreehand.Name = "btnFreehand";
+            this.btnFreehand.Size = new System.Drawing.Size(75, 23);
+            this.btnFreehand.TabIndex = 0;
+            this.btnFreehand.Text = "Freehand";
+            this.btnFreehand.UseVisualStyleBackColor = true;
+            this.btnFreehand.Click += new System.EventHandler(this.btnFreehand_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox.Location = new System.Drawing.Point(247, 27);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(571, 469);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // objectMove
+            // 
+            this.objectMove.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.objectMove.Location = new System.Drawing.Point(247, 0);
+            this.objectMove.Name = "objectMove";
+            this.objectMove.Size = new System.Drawing.Size(70, 38);
+            this.objectMove.TabIndex = 3;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 497);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainForm";
+            this.Text = "SequenceDiagram";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tracker1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnFreehand;
+        private System.Windows.Forms.Button btnText;
+        private System.Windows.Forms.Button btnActor;
+        private System.Windows.Forms.TrackBar tracker1;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Button btnArc;
+        private System.Windows.Forms.Button btnNewRect;
+        private System.Windows.Forms.Button btnDashLine;
+        private System.Windows.Forms.Button btnTextFont;
+        private System.Windows.Forms.Panel objectMove;
+    }
+}
+
---
+++ SequenceDiagram/CourseWorkSequenceDiagram/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseWorkSequenceDiagram
+{
+    public partial class mainForm : Form
+    {
+        Pen blackPen = new Pen(Color.Black);
+        SolidBrush brush = new SolidBrush(Color.Ivory);
+        SolidBrush drawBrush = new SolidBrush(Color.Black);
+        private bool pressed = false;
+
+        Graphics myGraph;
+        private Point startPosition, endPosition;
+
+        private String currentOperation = String.Empty;
+
+        public int width;
+        public int height;
+        private Bitmap bmp;
+        private string printText;
+        private string fName = "";
+        private Point firstPoint = new Point();
+        private Font textFont = new Font("Arial", 14);
+
+        public mainForm()
+        {
+            InitializeComponent();
+
+            //for save
+            bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            myGraph = Graphics.FromImage(bmp);
+            myGraph.FillRectangle(new SolidBrush(Color.White), 0, 0, bmp.Width, bmp.Height);
+            pictureBox.Image = bmp;
+
+            INIT();
+        }
+        private void btnText_Click(object sender, EventArgs e)
+        {
+            printText = "";
+        }
+
+        private void btnText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Point startP = new Point(startPosition.X, startPosition.Y);
+            printText += e.KeyChar;
+            myGraph = pictureBox.CreateGraphics();
+            myGraph.DrawString(printText, textFont, drawBrush, startP);
+        }
+        private void btnRectangle_Click(object sender, EventArgs e)
+        {
+            currentOperation = "Rectangle";
+        }
+        private void btnFreehand_Click(object sender, EventArgs e)
+        {
+            currentOperation = "Freehand";
+        }
+        private void btnActor_Click(object sender, EventArgs e)
+        {
+            currentOperation = "Actor";
+        }
+        private void tracker1_Scroll(object sender, EventArgs e)
+        {
+            blackPen.Width = tracker1.Value;
+        }
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            currentOperation = "Line";
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            //  toolStripStatusLabel3.Text = e.X.ToString() + " x " + e.Y.ToString();
+
+            if (currentOperation == "Freehand" && pressed == true)
+            {
+                endPosition = e.Location;
+                // myGraph = frmPictureBox.CreateGraphics();
+                myGraph.DrawLine(blackPen, startPosition, endPosition);
+                startPosition = endPosition;
+                pictureBox.Invalidate();
+            }
+        }
+        public void INIT()
+        {
+            objectMove.MouseDown += (ss, ee) =>
+            {
+                if (ee.Button == MouseButtons.Left) { firstPoint = Control.MousePosition; }
+            };
+            objectMove.MouseMove += (ss, ee) =>
+            {
+                if (ee.Button == MouseButtons.Left)
+                {
+                    Point temp = Control.MousePosition;
+                    Point result = new Point(firstPoint.X - temp.X, firstPoint.Y - temp.Y);
+
+                    objectMove.Location = new Point(objectMove.Location.X - result.X, objectMove.Location.Y - result.Y);
+
+                    firstPoint = temp;
+                }
+            };
+        }
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            pressed = false;
+
+            endPosition.X = e.X;
+            endPosition.Y = e.Y;
+
+            width = Math.Abs(endPosition.X - startPosition.X);
+            height = Math.Abs(endPosition.Y - startPosition.Y);
+
+            if (currentOperation == "Actor")
+            {
+                //  myGraph = frmPictureBox.CreateGraphics();
+                // head
+                int headHeight = (int)Math.Round(1.0 / 4.0 * height);
+                int headWidth = (int)Math.Round(2.0 / 3.0 * width) - (int)Math.Round((1.0 / 3.0) * width);
+                myGraph.DrawEllipse(blackPen, startPosition.X + (int)Math.Round((1.0 / 3.0) * width),
+                      startPosition.Y, headWidth, headHeight);
+
+                //body
+                myGraph.DrawLine(blackPen, startPosition.X + width / 2, startPosition.Y + headHeight,
+                      startPosition.X + width / 2, endPosition.Y - (int)Math.Round(3.0 / 8.0 * height));
+
+                //arms
+                myGraph.DrawLine(blackPen, startPosition.X, startPosition.Y + (int)Math.Round(3.0 / 8.0 * height),
+                    endPosition.X - width / 2, endPosition.Y - (int)Math.Round(5.0 / 8.0 * height));
+                myGraph.DrawLine(blackPen, startPosition.X + width / 2, startPosition.Y + (int)Math.Round(3.0 / 8.0 * height),
+                    endPosition.X, endPosition.Y - (int)Math.Round(5.0 / 8.0 * height));
+
+                //legs
+                myGraph.DrawLine(blackPen, startPosition.X + width / 2, endPosition.Y - (int)Math.Round(3.0 / 8.0 * height),
+                    startPosition.X + (int)Math.Round(1.0 / 5.0 * width), endPosition.Y);
+                myGraph.DrawLine(blackPen, startPosition.X + width / 2, endPosition.Y - (int)Math.Round(3.0 / 8.0 * height),
+                     startPosition.X + (int)Math.Round(4.0 / 5.0 * width), endPosition.Y);
+
+                pictureBox.Invalidate();
+            }
+            else if (currentOperation == "Rectangle")
+            {
+                myGraph.FillRectangle(brush, startPosition.X, startPosition.Y, width, height);
+                myGraph.DrawRectangle(blackPen, startPosition.X, startPosition.Y, width, height);
+                pictureBox.Invalidate();
+            }
+            else if (currentOperation == "NewRectangle")
+            {
+                int newRect = (int)Math.Round(width / 2.5);
+                myGraph.DrawRectangle(blackPen, startPosition.X, startPosition.Y, newRect, height);
+                pictureBox.Invalidate();
+            }
+            else if (currentOperation == "Arc")
+            {
+                myGraph.DrawArc(blackPen, startPosition.X, startPosition.Y, width, height, 180, 180);
+                pictureBox.Invalidate();
+            }
+            else if (currentOperation == "Line")
+            {
+                blackPen.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+                myGraph.DrawLine(blackPen, startPosition.X, startPosition.Y, endPosition.X, endPosition.Y);
+                pictureBox.Invalidate();
+                blackPen.EndCap = System.Drawing.Drawing2D.LineCap.Custom;
+            }
+            else if (currentOperation == "DashLine")
+            {
+                blackPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                //blackPen.EndCap = LineCap.ArrowAnchor;
+                myGraph.DrawLine(blackPen, startPosition.X, startPosition.Y, endPosition.X, endPosition.Y);
+                pictureBox.Invalidate();
+                blackPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            }
+        }
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            pressed = true;
+
+            startPosition.X = e.X;
+            startPosition.Y = e.Y;
+        }
+
+        private void newToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            fName = "";
+            bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            myGraph = Graphics.FromImage(bmp);
+            myGraph.FillRectangle(new SolidBrush(Color.White), 0, 0, pictureBox.Width, pictureBox.Height);
+            pictureBox.Image = bmp;
+        }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "JPEG|*.jpg|Bitmap|*.bmp|PNG|*.png";
+            ofd.ShowDialog();
+
+            if (ofd.FileName != "")
+            {
+                System.IO.FileStream stream = (System.IO.FileStream)ofd.OpenFile();
+                fName = ofd.FileName;
+                bmp = (Bitmap)Bitmap.FromStream(stream);
+                myGraph = Graphics.FromImage(bmp);
+                pictureBox.Image = bmp;
+                pictureBox.Invalidate();
+                stream.Close();
+
+            }
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is my course work! \n By: Stanislav Dimitrov");
+        }
+
+        private void btnArc_Click(object sender, EventArgs e)
+        {
+            currentOperation = "Arc";
+        }
+
+        private void btnNewRect_Click(object sender, EventArgs e)
+        {
+            currentOperation = "NewRectangle";
+        }
+
+        private void btnDashLine_Click(object sender, EventArgs e)
+        {
+            currentOperation = "DashLine";
+        }
+
+        private void btnTextFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                textFont = fontDialog.Font;
+                printText = " ";
+            }
+        }
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "JPEG|*.jpg|Bitmap|*.bmp|PNG|*.png";
+            sfd.ShowDialog();
+            fName = sfd.FileName;
+            if (sfd.FileName != "")
+            {
+                System.IO.FileStream stream = (System.IO.FileStream)sfd.OpenFile();
+
+                var extension = Path.GetExtension(sfd.FileName);
+                ImageFormat chosenFormat;
+                switch (extension.ToLower())
+                {
+                    case ".jpg":
+                        chosenFormat = ImageFormat.Jpeg;
+                        break;
+                    case ".png":
+                        chosenFormat = ImageFormat.Png;
+                        break;
+                    case ".bmp":
+                        chosenFormat = ImageFormat.Bmp;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(extension);
+                }
+                using (Bitmap bitmp = new Bitmap(pictureBox.Width, pictureBox.Height))
+                {
+                    pictureBox.DrawToBitmap(bitmp, new Rectangle(0, 0, pictureBox.Width, pictureBox.Height));
+                    bitmp.Save(stream, chosenFormat);
+                }
+                stream.Close();
+            }
+        }
+    }
+}
